b5f25a9072f304b81ff9e54de99629f98ddc5c3f
mattheussAL <matt_git@outlook.com>
2021-01-05 16:35:07 -0300

    Initial Setup

--- src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -1,7 +1,7 @@
 package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DsdeliverApplication {
